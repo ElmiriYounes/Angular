463367e85391b7d4f30a3cf974834e9aeebb4e0e
ElmiriYounes <elmiri.younes@hotmail.com>
2023-04-20 14:22:23 +0200

    fixing constants image from /constant/images.ts: absolute path

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,4 +1,6 @@
-export const heroBackground: string = `${document.location.origin}/assets/images/machines.jpg`;
-export const machineIcon: string = `${document.location.origin}/assets/images/machine.png`;
-export const shirt: string = `${document.location.origin}/assets/images/shirt.png`;
-export const card: string = `${document.location.origin}/assets/images/card.png`;
+// on deploy, assets is inside the root
+// on dev src is like the root so we do not need relatif path
+export const heroBackground: string = `assets/images/machines.jpg`;
+export const machineIcon: string = `assets/images/machine.png`;
+export const shirt: string = `assets/images/shirt.png`;
+export const card: string = `assets/images/card.png`;
